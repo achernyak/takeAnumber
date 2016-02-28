6016d2fb5baca8bc023d329cbdedc42862074ca9
achernyak <artemchernyak@gmail.com>
2016-02-27 22:36:11 -0600

    db config

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,13 @@ use Mix.Config
 # which you typically run after static files are built.
 config :takeAnumber, TakeAnumber.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "takeanumber.herokuapp.com", port: 80],
+  url: [scheme: "https", host: "takeanumber.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :takeAnumber, TakeAnumber.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
