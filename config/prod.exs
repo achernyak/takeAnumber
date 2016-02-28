d95e6d5508048f23b32e08993d180017cdd798fe
achernyak <artemchernyak@gmail.com>
2016-02-27 22:26:15 -0600

    fixed typo

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :takeAnumber, TakeAnumber.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "takeanumber.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
